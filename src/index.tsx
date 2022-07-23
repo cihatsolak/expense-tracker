09f5a57a811537f93ba2949759acf8322e5525f1
cihatsolak <cihatsolak@hotmail.com>
2022-07-23 18:18:10 +0300

    Configure user reducer

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
 import 'antd/dist/antd.css';
+
+import App from './App';
+import rootReducer from './store';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
