1a36bcfdb8b8da77aca6bf0480e22e61b0283904
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-07-27 23:28:06 +0300

    create registration page and service integration

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -1,11 +1,12 @@
-import { Button, Form, Input, InputNumber } from 'antd';
+import { Button, Form, Input, message, Space, InputNumber } from 'antd';
+import { useNavigate } from 'react-router-dom';
+import api from '../utils/api';
 
 const layout = {
     labelCol: { span: 8 },
     wrapperCol: { span: 16 },
 };
 
-/* eslint-disable no-template-curly-in-string */
 const validateMessages = {
     required: '${label} is required!',
     types: {
@@ -16,15 +17,27 @@ const validateMessages = {
         range: '${label} must be between ${min} and ${max}',
     },
 };
-/* eslint-enable no-template-curly-in-string */
+
+const showError = (errorMessage: string) => {
+    message.error(errorMessage);
+};
 
 const SignUp = () => {
-    const onFinish = (values: any) => {
-        console.log(values);
+    const navigate = useNavigate();
+    const onFinish = async (values: any) => {
+        try {
+            await api.post("/users/register", values);
+            navigate('/login');
+        }
+        catch (err: any) {
+            console.log({ err });
+            showError(err.response.data.errorMessage)
+        }
     };
 
     return (
         <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
+            <h2 style={{ textAlign: 'center', marginBottom: 20 }}>Register for an account</h2>
             <Form.Item name="username" label="Username" rules={[{ required: true }]}>
                 <Input />
             </Form.Item>
