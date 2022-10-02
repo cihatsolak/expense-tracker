208e80cda4e84de0e8dafcdd8db3c68828bc3eb4
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-10-02 22:52:18 +0300

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Register from './pages/register/Register'
+import Navbar from './components/navbar/Navbar'
+import Container from '@mui/material/Container';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Navbar />
+        <Container>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </Container>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/register/Register.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Register.module.css'
+
+export default function Register() {
+  return (
+    <div>Register</div>
+  )
+}
