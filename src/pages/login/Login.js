2a708ed321bb6236817affbb2952d96a4cb7821d
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-10-02 22:58:59 +0300

    Create the design of the login page

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Login.module.css'
+import { Container, Typography, Button, FormControl, FilledInput, InputLabel } from '@mui/material'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <Container>
+      <form>
+        <Typography sx={{ mt: 15, ml: 5, fontWeight: 'bold' }} variant="h4" color="darkslateblue">Login</Typography>
+
+        <FormControl fullWidth sx={{ mt: 5 }}>
+          <InputLabel htmlFor="email">Email</InputLabel>
+          <FilledInput
+            id="email"
+            label="Email"
+          />
+        </FormControl>
+
+        <FormControl fullWidth sx={{ my: 5 }}>
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <FilledInput
+            id="password"
+            label="password"
+          />
+        </FormControl>
+        <Button variant="outlined" type="submit" color="info" size="large" sx={{ mt: 5 }}>Login</Button>
+      </form>
+    </Container>
   )
 }
