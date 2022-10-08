a4f8811033c635a792b4338b31fb5d03fc857dfe
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-10-08 11:16:40 +0300

    Perform a login submission

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.module.css'
 import { Container, Typography, Button, FormControl, FilledInput, InputLabel } from '@mui/material'
 
+
 export default function Login() {
+
+  const [formInputs, setFormInputs] = useState({
+    email: '',
+    password: ''
+  })
+
+  const handleChange = (prop) => (event) => {
+    setFormInputs({ ...formInputs, [prop]: event.target.value })
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formInputs);
+  }
+
   return (
     <Container>
-      <form>
+      <form onSubmit={handleSubmit}>
         <Typography sx={{ mt: 15, ml: 5, fontWeight: 'bold' }} variant="h4" color="darkslateblue">Login</Typography>
 
         <FormControl fullWidth sx={{ mt: 5 }}>
@@ -13,6 +29,8 @@ export default function Login() {
           <FilledInput
             id="email"
             label="Email"
+            value={formInputs.email}
+            onChange={handleChange('email')}
           />
         </FormControl>
 
@@ -21,6 +39,8 @@ export default function Login() {
           <FilledInput
             id="password"
             label="password"
+            value={formInputs.password}
+            onChange={handleChange('password')}
           />
         </FormControl>
         <Button variant="outlined" type="submit" color="info" size="large" sx={{ mt: 5 }}>Login</Button>
